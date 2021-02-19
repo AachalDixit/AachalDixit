df765184c081f54ffec554434a11b04266b54275
AachalDixit <76690729+AachalDixit@users.noreply.github.com>
2021-02-19 12:34:03 +0530

    Add files via upload

---
+++ JS/fun.js
@@ -0,0 +1,8 @@
+function outputResult(result, text) {
+    currentResultOutput.textContent = result;
+    currentCalculationOutput.textContent = text;
+  }
+let number=0;
+number=(number+10)*2
+let text='20';
+outputResult(number,text);
